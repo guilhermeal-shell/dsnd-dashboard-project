ec7facf29b781d38d1d9c358336d328acf4fe710
Guilherme <78359464+guilhermeal-shell@users.noreply.github.com>
2025-07-02 15:23:16 +0200

    Update team.py

--- python-package/employee_events/team.py
+++ python-package/employee_events/team.py
@@ -1,30 +1,40 @@
 # Import the QueryBase class
 # YOUR CODE HERE
+from .query_base import QueryBase
 
 # Import dependencies for sql execution
 #### YOUR CODE HERE
+import sqlite3
+import pandas as pd
+
+connection = sqlite3.connect('python-package/employee_events/employee_events.db')
 
 # Create a subclass of QueryBase
 # called  `Team`
 #### YOUR CODE HERE
-
+class Team(QueryBase):
     # Set the class attribute `name`
     # to the string "team"
     #### YOUR CODE HERE
-
+    name = "team"
 
     # Define a `names` method
     # that receives no arguments
     # This method should return
     # a list of tuples from an sql execution
     #### YOUR CODE HERE
-        
+    def names(self):
         # Query 5
         # Write an SQL query that selects
         # the team_name and team_id columns
         # from the team table for all teams
         # in the database
         #### YOUR CODE HERE
+        sql_query = """
+            SELECT team_name, team_id
+            FROM team
+        """
+        return self.query(sql_query)
     
 
     # Define a `username` method
@@ -32,7 +42,7 @@
     # This method should return
     # a list of tuples from an sql execution
     #### YOUR CODE HERE
-
+    def username(self, id):
         # Query 6
         # Write an SQL query
         # that selects the team_name column
@@ -40,7 +50,12 @@
         # to only return the team name related to
         # the ID argument
         #### YOUR CODE HERE
-
+        sql_query = """
+            SELECT team_name
+            FROM team
+            WHERE team_id = ?
+        """
+        return self.query(sql_query)
 
     # Below is method with an SQL query
     # This SQL query generates the data needed for
@@ -52,7 +67,7 @@
     #### YOUR CODE HERE
     def model_data(self, id):
 
-        return f"""
+        sql_query = f"""
             SELECT positive_events, negative_events FROM (
                     SELECT employee_id
                          , SUM(positive_events) positive_events
@@ -64,3 +79,5 @@
                     GROUP BY employee_id
                    )
                 """
+        return pd.read_sql_query(sql_query, connection)
+        
